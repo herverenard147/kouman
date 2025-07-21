9a2c3606cb147f6966f6c2e93b66fe5e780fa295
herverenard147 <herverenard147@gmail.com>
2025-07-21 08:53:30 +0000

    modif seed

--- database/seeders/AvisSeeder.php
+++ database/seeders/AvisSeeder.php
@@ -11,9 +11,9 @@ class AvisSeeder extends Seeder
     public function run(): void
     {
         $avis = [
-            ['note' => 4.5, 'commentaire' => 'Très bon séjour', 'idClient' => 1, 'idHebergement' => 1, 'idExcursion' => 0],
-            ['note' => 3.0, 'commentaire' => 'Correct mais peut mieux faire', 'idClient' => 2, 'idHebergement' => 0, 'idExcursion' => 1],
-            ['note' => 5.0, 'commentaire' => 'Expérience parfaite !', 'idClient' => 3, 'idHebergement' => 2, 'idExcursion' => 0],
+            ['note' => 4.5, 'commentaire' => 'Très bon séjour', 'idClient' => 1, 'idHebergement' => 1],
+            ['note' => 3.0, 'commentaire' => 'Correct mais peut mieux faire', 'idClient' => 2, 'idHebergement' => 0],
+            ['note' => 5.0, 'commentaire' => 'Expérience parfaite !', 'idClient' => 3, 'idHebergement' => 2],
             ['note' => 5.0, 'commentaire' => 'Expérience parfaite !', 'idClient' => 3, 'idHebergement' => 0, 'idExcursion' => 2],
         ];
 
